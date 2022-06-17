327796faeac855cf7b6f4a81df5553b091803ad9
PieroMC <piero0716.mc@gmail.com>
2022-06-17 02:00:47 -0500

    rama elias tabla

--- views/template/nav-alumno.view.php
+++ views/template/nav-alumno.view.php
@@ -1,6 +1,6 @@
 <nav class="navegacion">
     <ul class="lista">
-        <li class="primary"><a href="">Matricula</a></li>
+        <li class="primary"><a href="alumno_matricula.php">Matricula</a></li>
         <li class="primary"><a href="">Notas</a></li>
         <li class="primary"><a href="">Docentes</a></li>
         <li class="primary"><a href="./index.php">Salir</a></li>
